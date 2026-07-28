a407ad3459510d01b0297ad93b3e8f5c2b89fab1
ellensilva-dev <linoellenlinosilva@gmail.com>
2026-07-27 21:14:19 -0300

    w292

--- app/main.py
+++ app/main.py
@@ -24,11 +24,18 @@ sorted_variables = {
     "immutable": [],
 }
 
-sorted_variables["mutable"] = [my_favourite_films, marks, collection_of_coins]
-sorted_variables["immutable"] = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    profile_info,
-]
+sorted_variables = {
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins,
+    ],
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info,
+    ],
+}
+
